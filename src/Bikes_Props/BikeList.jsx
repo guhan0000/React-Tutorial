03ec4563b913ab53899e51bd4b0346b78fcb488f
guhan0000 <guhanm0000@gmail.com>
2025-11-09 21:45:11 +0530

    sorting & filtering performed

--- src/Bikes_Props/BikeList.jsx
+++ src/Bikes_Props/BikeList.jsx
@@ -8,16 +8,18 @@ import honda from "../assets/honda.avif";
 function BikeList() {
   const bikeList = [
     {
-      // brand: "TVS",
-      // model: "Raider",
-      // price: "₹1,15,000",
-      // image: tvs,
-      // show: true,
+      brand: "TVS",
+      model: "Raider",
+      price: "₹1,15,000",
+      ex_cost: 115000,
+      image: tvs,
+      show: true,
     },
     {
       brand: "Yamaha",
       model: "MT-15",
       price: "₹2,15,000",
+      ex_cost: 215000,
       image: yamaha,
       show: true,
     },
@@ -25,6 +27,7 @@ function BikeList() {
       brand: "Bajaj",
       model: "NS-200",
       price: "₹1,55,000",
+      ex_cost: 155000,
       image: bajaj,
       show: true,
     },
@@ -32,11 +35,22 @@ function BikeList() {
       brand: "Honda",
       model: "SP-125",
       price: "₹1,00,000",
+      ex_cost: 100000,
       image: honda,
       show: true,
     },
   ];
-  const myBikeList = bikeList.map((bike) => (
+  //sorts in ascending order
+  const sortedBikesAsc = [...bikeList].sort(
+    (bike1, bike2) => bike1.ex_cost - bike2.ex_cost
+  );
+  // sorts in descending order
+  const sortedBikesDesc = bikeList
+    .slice()
+    .sort((bike1, bike2) => bike2.ex_cost - bike1.ex_cost);
+  // filters bikes which are lesser than 2Lakhs
+  const bikeFilter = bikeList.filter((bike) => bike.ex_cost < 200000);
+  const myBikeList = bikeFilter.map((bike) => (
     <Bike
       brand={bike.brand}
       model={bike.model}
