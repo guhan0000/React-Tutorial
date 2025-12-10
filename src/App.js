d0aad1899fc04d4cfb9185de24c86cc501805716
guhan0000 <guhanm0000@gmail.com>
2025-12-10 12:20:18 +0530

    Dom using useRef

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@
 // import SearchData from "./api_fetch/SearchData";
 import "./App.css";
 import { React, useRef } from "react";
-// import useRefDOM from "./useRef/UseRefDemo";
-// import InputFocus from "./useRef/InputFocus";
-// import RenderCount from "./useRef/RenderCount";
-import UseRefDemo1 from "./useRef/UseRefDemo1";
-import StateVsRef from "./useRef/StateVsRef";
+
+import UseRefDom from "./useRef/UseRefDom";
 
 function App() {
   return (
     <>
-     <StateVsRef/>
+      <UseRefDom></UseRefDom>
     </>
   );
 }
---
+++ src/useRef/UseRefDom.jsx
@@ -0,0 +1,21 @@
+import React, { useRef, useState } from "react";
+
+const UseRefDom = () => {
+  // const [count, setCount] = useState(0);
+  let count = "Zero";
+  let h1Ref = useRef();
+  return (
+    <div>
+      <h1 ref={h1Ref}></h1>
+      <button
+        onClick={() => {
+          h1Ref.current.innerHTML = `<em>${count}</em>`;
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
+};
+
+export default UseRefDom;
